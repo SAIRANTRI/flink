8ae8f5206bc14b44f27d6e37e3e91976e8211ca9
SAIRANTRI <pathaksairantri@gmail.com>
2025-01-13 12:28:50 +0530

    added sidebar and functionalities to fetch the user from the database with additional info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
     
-        <div>
-          <SignedOut>
-            <SignInButton mode="modal">
-            <Button>Sign In</Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <Button variant={"secondary"}>Click me</Button>
+        <div className="m-4">
+          <h1>Home Page</h1>
         </div>
   );
 }
